794ab0cdd283b3e8c725cc7563411ffd555f1e5f
Luis <luis_henrique_vieira@hotmail.com>
2017-07-06 20:32:22 -0300

    commit

--- VerAlbumLogado.php
+++ VerAlbumLogado.php
@@ -14,7 +14,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo "Usuarios"
+            echo "Album"
         ?>
         <script src="js/bootstrap.min.js"></script>
     </body>
